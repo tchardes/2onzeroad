581057b98c807c1df473f85167d534b8504aded7
Alexandre CHARDES <Alexandre CHARDES>
2016-10-11 21:51:13 +0200

    test2

--- assets/libs/js/map.js
+++ assets/libs/js/map.js
@@ -99,11 +99,7 @@
 // }
 
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-  console.log("relou");
-}
+
+    map = new OpenLayers.Map("map");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.zoomToMaxExtent();
