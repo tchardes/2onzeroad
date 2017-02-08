28deb0b02dbf9ce711ab0496dabd7a7f4bc9a98b
achardes <alexandre.chardes@gmail.com>
2017-02-08 20:02:50 +0100

    mosaic

--- assets/libs/js/mosaic.js
+++ assets/libs/js/mosaic.js
@@ -1,11 +1,7 @@
 function initMosaic(folderPath) {
     var allImages = "";
 
-    for (var i = 1; i <= 36; i++) {
-        allImages += '<img src="'+folderPath+'/mosaic_'+i+'.png">';
-    }
-
-    for (var i = 1; i <= 36; i++) {
+    for (var i = 1; i <= 72; i++) {
         allImages += '<img src="'+folderPath+'/mosaic_'+i+'.png">';
     }
 
